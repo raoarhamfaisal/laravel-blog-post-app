ecd38c2a76355895bb2d487a98da739735ecc135
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-09-16 22:43:56 +0500

    Adding Blog post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -14,9 +15,14 @@ use App\Http\Controllers\UserController;
 |
 */
 
+
+// Users routes
 Route::get('/', [UserController::class, "showCorrectHomepage"]);
-
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+
+// Post routes
+Route::get('/create-post', [PostController::class, 'showCreatePost']);
+Route::post('/create-post', [PostController::class, 'storeCreatePost']);
