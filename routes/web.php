98b6dec9d44b7b23149f9b5dd0dc5b7890ebda89
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-09-17 01:00:06 +0500

    Adding middleware

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use App\Http\Controllers\UserController;
 
 
 // Users routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');;
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
 
 
 // Post routes
-Route::get('/create-post', [PostController::class, 'showCreatePost']);
-Route::post('/create-post', [PostController::class, 'storeCreatePost']);
-Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+Route::get('/create-post', [PostController::class, 'showCreatePost'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'storeCreatePost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost'])->middleware('mustBeLoggedIn');
